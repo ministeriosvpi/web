cd9b9bb4fda3aae44de4a0db94fa5288d1b11a50
gamarcano <70113905+gamarcano@users.noreply.github.com>
2023-06-30 14:03:53 -0400

    update

---
+++ test/test2 - copia.php
@@ -0,0 +1,25 @@
+
+    <!--
+      ****************************
+      ZONA DE TESTING
+      ZONA DE TESTING
+      ↓↓↓↓↓↓
+    -->
+
+
+
+
+
+    <?php echo "test2.PHP Ejemplo"; ?>
+
+
+
+
+
+
+    <!--
+      ↑↑↑↑↑↑↑↑
+      ZONA DE TESTING
+      ZONA DE TESTING
+      ****************************
+    -->
