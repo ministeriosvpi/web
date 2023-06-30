cbd5d9effe1b8167b6959edeff77d03837d3f60d
gamarcano <70113905+gamarcano@users.noreply.github.com>
2023-06-30 12:12:44 -0400

    update

---
+++ test/index.php
@@ -0,0 +1,248 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Ministerios Vida Plena Internacional - Página principal</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="assets/vendor/venobox/venobox.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+
+  <!-- Google Ads -->
+  <script data-ad-client="ca-pub-3783384967008146" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+
+
+</head>
+
+<body>
+
+  <!-- ======= Top Bar ======= -->
+  <div id="topbar" class="d-none d-lg-flex align-items-center fixed-top">
+    <div class="container d-flex">
+      <div class="contact-info me-auto">
+        <i class="icofont-envelope"></i> <a href="mailto:info@ministeriosvpi.com" target="_blank">info@ministeriosvpi.com</a>  
+        <i class="icofont-whatsapp"></i> <a href="https://wa.me/+584124446916" target="_blank"> +58 412 444 6916</a>
+      </div>
+      <div class="social-links">
+        <a href="https://www.youtube.com/c/IglesiaVidaPlenaInternacional" class="instagram" target="_blank"><i class="icofont-youtube"></i></a>
+        <a href="https://twitter.com/iglesiavpi" class="twitter" target="_blank"><i class="icofont-twitter"></i></a>
+        <a href="https://www.facebook.com/iglesiavpi" class="facebook" target="_blank"><i class="icofont-facebook"></i></a>
+        <a href="https://www.instagram.com/iglesiavpi/" class="instagram" target="_blank"><i class="icofont-instagram"></i></a>
+        <!-- a href="#" class="skype"><i class="icofont-skype"></i></a -->
+        <!-- a href="#" class="linkedin"><i class="icofont-linkedin"></i></i></a -->
+      </div>
+    </div>
+  </div>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top">
+    <div class="container d-flex align-items-center">
+
+      <!-- h1 class="logo me-auto"><a href="index.html">Ministerios Vida Plena Internacional<span>.</span></a></h1 -->
+      <!-- Uncomment below if you prefer to use an image logo -->
+      <a href="index.html" class="logo me-auto"><img src="assets/img/vpi/vpi-logo.png" alt=""></a>
+
+
+
+
+      <!-- NAV MENU -->
+      <!-- NAV MENU -->
+      <!-- NAV MENU -->
+      <!-- NAV MENU -->
+      <!-- NAV MENU -->
+      <nav class="nav-menu d-none d-lg-block">
+        <ul>
+          <li class="active"><a href="index.html">Inicio</a></li>
+          <li><a href="donar/index.html">Donar</a></li>
+          <li><a href="#about">Quienes somos</a></li>
+          <li><a href="#services">Servicios</a></li>
+          <li><a href="#ensenanzas">Enseñanzas</a></li>
+          <li><a href="escuela_html/index.html">Escuela online</a></li>
+          <li><a href="#como-llegar">Cómo llegar</a></li>
+          <li><a href="#contact">Contacto</a></li>
+          <li><a href="tienda/index.html">Tienda</a></li>
+          <li class="drop-down"><a href="#">Enlaces</a>
+            <ul>
+              <li class="drop-down"><a href="#social">Redes sociales</a>
+                <ul>
+                  <li><a href="https://www.youtube.com/c/IglesiaVidaPlenaInternacional" target="_blank">Youtube</a></li>
+                  <li><a href="https://twitter.com/iglesiavpi" target="_blank">Twitter</a></li>
+                  <li><a href="https://www.facebook.com/iglesiavpi" target="_blank">Facebook</a></li>
+                  <li><a href="https://www.instagram.com/iglesiavpi/" target="_blank">Instagram</a></li>
+                </ul>
+              </li>
+              <li><a href="fundacion/index.html">Fundación "El Reino está aquí"</a></li>
+              <li><a href="jovenes/index.html">Ministerio de jóvenes</a></li>
+              <li><a href="aliento/index.html">Aliento de vida</a></li>
+              <li><a href="casas/index.html">Casas de paz</a></li>
+              <li><a href="podcast/index.html">Podcast</a></li>
+              <li><a href="noticias/index.html">Noticias</a></li>
+              <!--li><a href="tienda/index.html">Tienda</a></li-->
+              <li><a href="#como-llegar">Visítanos</a></li>
+            </ul>
+          </li>
+        </ul>
+      </nav>
+      <!-- NAV MENU -->
+      <!-- NAV MENU -->
+      <!-- NAV MENU -->
+      <!-- NAV MENU -->
+      <!-- NAV MENU -->
+
+
+
+
+
+    </div>
+  </header><!-- End Header -->
+
+
+  <main id="main">
+
+
+
+
+  </main><!-- End #main -->
+
+
+
+
+
+
+
+
+
+  <!-- ======= Footer ======= -->
+  <!-- ======= Footer ======= -->
+  <!-- ======= Footer ======= -->
+  <!-- ======= Footer ======= -->
+  <!-- ======= Footer ======= -->
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+    <div class="container py-4">
+      <!-- BARRA SEPARADORA DEL FOOTER -->
+    </div>
+
+    <div class="footer-top">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-lg-3 col-md-6 footer-contact">
+            <h3>Ministerios<br>Vida Plena Internacional<span></span></h3>
+            <p>
+              Avenida 102, Centro Comercial Pin Aragua, Planta baja, Barrio La Coromoto, 2104, Maracay, Aragua, Venezuela.<br>
+              <i>(Detrás del Diario el Siglo, Al Lado del Supermercado Ali Fung)</i><br><br>
+              <strong>Teléfono:</strong> +58 412 444 6916<br>
+              <strong>Email:</strong> info@ministeriosvpi.com<br><br>
+
+              <strong>Webmail:</strong> <a href="https://www.ministeriosvpi.com/webmail" target="_blank"> <i class="icofont-at"></i>ministeriosvpi</a>
+
+
+            </p>
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-links">
+            <h4>Menú principal</h4>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="index.html">Inicio</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="donar/index.html">Donar</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#about">Quienes somos</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#services">Servicios</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#ensenanzas">Enseñanzas</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="escuela_html/index.html">Escuela online</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#como-llegar">Cómo llegar</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#contact">Contacto</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="tienda/index.html">Tienda</a></li>              
+            </ul>
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-links">
+            <h4>Enlaces directos</h4>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="#social">Redes sociales</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="fundacion/index.html">Fundación "El Reino está aquí"</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="jovenes/index.html">Ministerio de jóvenes</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="aliento/index.html">Aliento de vida</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="casas/index.html">Casas de paz</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="podcast/index.html">Podcast</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="noticias/index.html">Noticias</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#como-llegar">Visítanos</a></li>
+            </ul>
+          </div>
+
+
+          <div class="col-lg-3 col-md-6 footer-links">
+            <h4>Visita nuestras redes sociales</h4>
+            <p>Síguenos en nuestras redes sociales y disfruta de todo el contenido que tenemos para tí</p>
+            <div class="social-links mt-3">
+              <a href="https://www.youtube.com/c/IglesiaVidaPlenaInternacional" class="youtube" target="_blank"><i class="bx bxl-youtube"></i></a>
+              <a href="https://twitter.com/iglesiavpi" class="twitter" target="_blank"><i class="bx bxl-twitter"></i></a>
+              <a href="https://www.facebook.com/iglesiavpi" class="facebook" target="_blank"><i class="bx bxl-facebook"></i></a>
+              <a href="https://www.instagram.com/iglesiavpi" class="instagram" target="_blank"><i class="bx bxl-instagram"></i></a>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+    <div class="container py-4">
+      <div class="copyright">
+        &copy; Copyright - <strong><span>Sistemas e Informática. Ministerios Vida Plena Internacional.</span></strong> Todos los derechos reservados. 2021.
+      </div>
+    </div>
+  </footer>
+  <!-- End Footer -->
+  <!-- End Footer -->
+  <!-- End Footer -->
+  <!-- End Footer -->
+  <!-- End Footer -->
+  <!-- End Footer -->
+
+
+
+
+
+  
+
+  <div id="preloader"></div>
+  <a href="#" class="back-to-top"><i class="icofont-simple-up"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/jquery/jquery.min.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/jquery.easing/jquery.easing.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <script src="assets/vendor/waypoints/jquery.waypoints.min.js"></script>
+  <script src="assets/vendor/counterup/counterup.min.js"></script>
+  <script src="assets/vendor/owl.carousel/owl.carousel.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/venobox/venobox.min.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
